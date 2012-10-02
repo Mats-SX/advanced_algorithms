03cbb617fd8cc747a04d650a49a3893e5c3890d5
Mats SX <Mats.SXz@gmail.com>
2012-10-02 13:08:39 +0200

    Changed name of project

---
+++ java_workspace/EDAN55_labs/src/lab4/Main.java
@@ -0,0 +1,83 @@
+package lab4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CollatzSimulator sim = new CollatzSimulator();
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.println("Which algorithm?");
+			int alg = scan.nextInt();
+			System.out.println("Give me the sequence number!");
+			int N = scan.nextInt();
+			switch (alg) {
+			case 0:
+				Main.dictionary(sim, N);
+				break;
+			case 1:
+				Main.quadratic(sim, N);
+				break;
+			case 2:
+				Main.D(N);
+				break;
+			case 9:
+				Main.test(sim, N);
+				break;
+			default:
+				System.out.println("You did something wrong. Algorithms are 0, 1 or 2, ok?");
+				break;
+			}
+		}
+	}
+	
+	private static void test(CollatzSimulator sim, int N) {
+		Solver s = new Solver();
+		s.initRandVector();
+//		long lol = s.hash(N);
+		System.out.println((N & (1L << 2)));
+	}
+	
+	private static void dictionary(CollatzSimulator sim, int N) {
+		sim.createSequencesUpTo(N, 0);
+		System.out.println("---------------------------");
+		System.out.println("Max value: " + sim.getMaximumValue());
+		System.out.println("# distinct values: " + sim.getDistinctValues());
+		System.out.println("Length of sequence: " + sim.getLengthOfSequence());
+		System.out.println("###########################");
+	}
+	
+	private static void quadratic(CollatzSimulator sim, int N) {
+		long start = System.currentTimeMillis();
+		sim.createSequencesUpTo(N, 0);
+		long max = sim.getMaximumValue();
+		long stopTime = 0;
+		for (int i = 1; i <= max; i++){
+			sim.createSequencesUpTo(N, i);
+			stopTime = System.currentTimeMillis();
+			if (stopTime - start > 60000){
+				System.out.println("Timeout for N: " + N);
+				break;
+			}
+		}
+		System.out.println("Took the time: " + (stopTime - start));
+	}
+	
+	private static void D(int N) {
+		Solver s = new Solver();
+		System.out.println("How many times shall we run the algorithm?");
+		int iterations = new Scanner(System.in).nextInt();
+		double[] estimates = new double[iterations];
+		for (int i = 0; i < iterations; i++) {
+			estimates[i] = s.solve(N);
+			System.out.println("Iteration " + (i + 1) + " gave the estimate " + estimates[i]);
+		}
+		Arrays.sort(estimates);
+		System.out.println("After " + iterations + 
+				" iterations, we can conclude that the # distinct values is roughly " 
+				+ estimates[iterations/2]);
+	}
+
+}
---
+++ java_workspace/EDAN55_labs/src/lab4/Solver.java
@@ -0,0 +1,78 @@
+package lab4;
+
+import java.util.Random;
+import java.util.TreeMap;
+
+public class Solver {
+	private static final long R = Integer.MAX_VALUE;
+	private static final int t = 500;					// our memory size
+	private static final Random RAND = new Random();
+	private long r;
+	private int[] randomNbrs;
+	
+	/**
+	 * Algorithm D
+	 */
+	public double solve(int N) {
+		initRandVector();
+		TreeMap<Long, Long> map = new TreeMap<Long, Long>();
+		long sequence = 1;
+		long current = sequence;
+		while (true) {
+			long hash = hash2(current);
+//			System.out.println("The hash for " + current + " is " + hash);
+			if (map.size() < t) {
+//				System.out.println("Putting first t numbers into map");
+				map.put(hash, current);
+			} else if (hash < map.lastKey() && !map.containsKey(hash)) {
+//				System.out.println("Found a better hash for " + current);
+				map.pollLastEntry();
+				map.put(hash, current);
+			}
+			if (current == 1) {
+//				System.out.println("Reached the end of subsequence " + sequence);
+				sequence++;
+				if (sequence > N) {
+//					System.out.println("The end of it all!");
+					double d = ((double) (map.size() * R)) / map.lastKey();
+//					System.out.println("Estimated # distincts (|C_n|): " + d);
+					return d;
+				}
+				current = sequence;
+				continue;
+			}
+//			System.out.println("Getting new value from sequence " + sequence);
+			current = CollatzSimulator.nextInSequence(current);
+		}
+	}
+	
+	void initRandVector() {
+		randomNbrs = new int[64];
+		for(int i = 0; i < randomNbrs.length; i++){
+			randomNbrs[i] = RAND.nextInt(Integer.MAX_VALUE);
+		}
+	}
+
+	long hash(long x) {
+		String bits = Long.toBinaryString(x);
+		long hash = 0;
+		for (int i = 0; bits.length() > 0; i++) {
+			int bit = Integer.parseInt(bits.substring(bits.length() - 1));
+			if (bit == 1) {
+				hash = hash ^ randomNbrs[i];
+			}
+			bits = bits.substring(0, bits.length() - 1);
+		}
+		return hash;
+	}
+
+	long hash2(long x) {
+		long hash = 0;
+		for (int i = 0; i < randomNbrs.length; i++) {
+			if ((x & (1L << i)) != 0L) {
+				hash = hash ^ randomNbrs[i];
+			}			
+		}
+		return hash;
+	}
+}
