3db1052efd8227369be56eb27a2d6338159340ca
Mats SX <Mats.SXz@gmail.com>
2012-10-09 13:14:49 +0200

    Added a few instances. Implemented new design with three dimensions.

--- java_workspace/Rainbow Perfect Matchings/src/p1/Parser.java
+++ java_workspace/Rainbow Perfect Matchings/src/p1/Parser.java
@@ -3,8 +3,12 @@ package p1;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+
+import p1.Algorithm.Edge;
 
 /**
  * Either we use this or a Java class to create instances.
@@ -13,6 +17,7 @@ import java.util.Scanner;
  */
 public class Parser {
 	private Scanner scan;
+	public HashSet<Integer> colours;
 	
 	public Parser(String filename) {
 		try {
@@ -20,6 +25,77 @@ public class Parser {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/*
+	 * New and good
+	 * 
+	 */
+	public BigInteger[][][] parseWholeStructure() {
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		
+		/*
+		 * Initialize biadjacency matrix with all 0s
+		 */
+		BigInteger[][][] matrix = new BigInteger[n/2][n/2][n/2];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				for (int k = 0; k < matrix[i][j].length; k++) {
+					matrix[i][j][k] = BigInteger.ZERO;
+				}
+			}
+		}
+		
+		/*
+		HashSet<Integer> v1 = new HashSet<Integer>();
+		HashSet<Integer> v2 = new HashSet<Integer>();
+		HashSet<Edge> e = new HashSet<Edge>();
+		Edge[][][] eM = new Edge[n/2][n/2][n/2];
+		*/
+		colours = new HashSet<Integer>();
+		
+		/*
+		 * parsing all edges
+		 */
+		for (int i = 0; i < m; i++) {
+			int from = scan.nextInt();
+			int to = scan.nextInt();
+			int colour = scan.nextInt();
+			matrix[from - 1][to - n / 2 - 1][colour] = BigInteger.valueOf(Algorithm.RAND.nextInt(5) + 1);
+			colours.add(colour);
+		}
+		
+		return matrix;
+	}
+	
+	
+	// this is old and bad
+	public ArrayList<BigInteger[][]> parse() {
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		
+		BigInteger[][] matrix = new BigInteger[n/2][n/2];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				matrix[i][j] = BigInteger.ZERO;
+			}
+		}
+		ArrayList<BigInteger[][]> list = new ArrayList<BigInteger[][]>();
+		for (int i = 0; i < n / 2; i++) {
+			list.add(Arrays.copyOf(matrix, matrix.length));
+		}		
+		
+		for (int i = 0; i < m; i++) {
+			int from = scan.nextInt();
+			int to = scan.nextInt();
+			int colour = scan.nextInt();
+//			list.get(colour - 1)[from - 1][to - n / 2 - 1] = ;
+			matrix[from - 1][to - n / 2 - 1] = BigInteger.ONE;
+		}
+		
+		list.add(matrix);
+		return list;
 	}
 	
 	public BigInteger[][] parseBiAdjacencyMatrixNoColours() {
--- java_workspace/Rainbow Perfect Matchings/src/test/Test.java
+++ java_workspace/Rainbow Perfect Matchings/src/test/Test.java
@@ -9,13 +9,33 @@ import p1.Parser;
 public class Test {
 	
 	public static void main(String[] args) {
-		testDeterminant(1);
+//		testDeterminant(1);
 //		testPieceOne();
+		testNewParsingAndDeterminantTripleStructure();
+	}
+	
+	static void testNewParsingAndDeterminantTripleStructure() {
+		Parser p = new Parser("g6_2prm.txt");
+		BigInteger[][][] triple = p.parseWholeStructure();
+		for (int i = 0; i < triple.length; i++) {
+			for (int j = 0; j < triple[i].length; j++) {
+				System.out.print("[ ");
+				for (int k = 0; k < triple[i][j].length; k++) {
+					System.out.print(triple[i][j][k] + " ");
+				}
+				System.out.print("]");
+			}
+			System.out.println();
+		}
+		
+		Algorithm alg = new Algorithm();
+		BigInteger result = alg.determinant(triple, p.colours);
+		System.out.println(result);
 	}
 	
 	static void testDeterminant() {
 		Random rand = new Random();
-		int n =10;
+		int n = 10;
 		BigInteger[][] matrix = new BigInteger[n][n];
 		System.out.print("[");
 		for (int i = 0; i < n; i++) {
@@ -28,23 +48,22 @@ public class Test {
 		System.out.println("]");
 		
 		Algorithm alg = new Algorithm();
-		System.out.println(alg.determinant(matrix).toString());
+//		System.out.println(alg.determinant(matrix).toString());
 	}
 	
 	static void testDeterminant(int v) {
 		Random rand = new Random();
-		int n = 6;
+		int n = 10;
 		int[][] matrix = new int[n][n];
-//		System.out.print("{");
+		System.out.print("[");
 		for (int i = 0; i < n; i++) {
-//			System.out.print("{");
 			for (int j = 0; j < n; j++) {
 				matrix[i][j] = rand.nextInt(Algorithm.PRIME);
-				System.out.print(matrix[i][j] + ",");
+				System.out.print(matrix[i][j] + " ");
 			}
-			System.out.println("},");
+			System.out.println("; ");
 		}
-//		System.out.println("}");
+		System.out.println("]");
 		
 		Algorithm alg = new Algorithm();
 		System.out.println(alg.determinant(matrix));
@@ -53,7 +72,6 @@ public class Test {
 	static void testPieceOne() {
 		Parser p = new Parser("g10_true.txt");
 		Algorithm a = new Algorithm();
-		a.setInstance(p.parseBiAdjacencyMatrixNoColours());
-		System.out.println(a.pieceOne());
+//		System.out.println(a.pieceOne(p.parseBiAdjacencyMatrixNoColours()));
 	}
 }
